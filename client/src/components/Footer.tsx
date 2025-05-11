25d615796d54092ad8af44e50e88025616f4b82e
Jacob Watson <jacobwatson@Jacobs-MacBook-Pro-2.local>
2025-05-10 23:19:26 -0600

    worked with nav and footer. added new nav image for coming soon

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Footer: React.FC = () => {
     return (
         <footer className="bg-[#484848] text-[#ff7f1d] py-5">
-        <div>
-            <p className='flex flex-col items-center'>Fire and White LLC</p>
+        <div className="">
+            <p className='flex flex-col items-center mb-auto'>Fire and White LLC</p>
         </div>
         </footer>
     );
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import wordMark from '../assets/images/wordmark.png';
+// import wordMark from '../assets/images/wordmark.png';
+import logo from '../assets/images/header-image.png'
 
 
 const CustomNavbar: React.FC = () => {
@@ -14,39 +15,31 @@ const CustomNavbar: React.FC = () => {
 
     return (
 
-        <nav className="bg-[#484848] shadow-lg">
-            <div className="max-w-screen-xl mx-auto px-4 py-2 flex items-center justify-between">
-                {/* Word Mark as Logo */}
-                {/* <Link to="/" className="flex items-center">
-          <img src={wordMark} alt="Fire and White" height="40" />
-        </Link> */}
-
+        <nav className="bg-[#484848] shadow-lg min-h[120px">
                 {/* Navbar Links */}
-                <div className="hidden md:flex space-x-6">
-                    <Link to="/" className="">
+                <div className="max-w-screen-xl mx-auto px-4 py-1 flex justify-center">
+                    <Link to="/" className="block">
                         <img
-                            src={wordMark}
+                            src={logo}
                             alt="Fire and White"
-                            className="h-7 w-auto"
+                            className="h-[100px] w-auto"
                         />
                     </Link>
 
                     {/* <a href="mailto:admin@fireandwhite.com" className="text-gray-700 hover:text-blue-600">admin@fireandwhite.com</a> */}
-                    <Link to="/Episodes" className="text-gray-900 hover:text-orange-500">Episodes</Link>
+                    {/* <Link to="/Episodes" className="text-gray-900 hover:text-orange-500">Episodes</Link>
                     <Link to="/Blog" className="text-gray-200 hover:text-orange-500">Blog</Link>
                     <Link to="/login" className="text-gray-200 hover:text-orange-500">Login</Link>
-                    <Link to="/signup" className="text-gray-200 hover:text-orange-500">Sign Up</Link>
+                    <Link to="/signup" className="text-gray-200 hover:text-orange-500">Sign Up</Link> */}
                 </div>
 
-                {/* Mobile Toggle Button */}
-                <div className="md:hidden">
+                {/* <div className="md:hidden">
                     <button className="text-white focus:outline-none">
                         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
                         </svg>
                     </button>
-                </div>
-            </div>
+                </div> */}
         </nav>
     );
 }
